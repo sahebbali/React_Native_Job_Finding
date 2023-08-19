ce4509d8454ada21e3935a5cf4f3874728dfd78d
sahebbali <sahebbali253@gmail.comm>
2023-08-19 09:53:12 +0600

    add job details page

--- components/index.js
+++ components/index.js
@@ -4,9 +4,26 @@ import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
 import Wellcome from "./home/wellcome/Wellcome";
 import Popularjobs from './home/popular/Popularjobs.jsx';
 import Nearbyjobs from './home/nearby/Nearbyjobs'
+
+// job details screen
+import Company from "./jobdetails/company/Company";
+import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+import { default as JobAbout } from "./jobdetails/about/About";
+import { default as JobFooter } from "./jobdetails/footer/Footer";
+import Specifics from "./jobdetails/specifics/Specifics";
+
+// common
+import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+
 export {
   ScreenHeaderBtn,
   Wellcome,
   Popularjobs,
-  Nearbyjobs
+  Nearbyjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard,
 };
